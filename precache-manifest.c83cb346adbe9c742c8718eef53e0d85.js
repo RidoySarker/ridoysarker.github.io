f718597ffdc28169a5f3777b9619161c24bb67b5
Skr@&25802 <csridoy42@gmail.com>
2020-08-06 02:33:00 +0600

    Updates

---
+++ precache-manifest.c83cb346adbe9c742c8718eef53e0d85.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327371860d2cddec23387a687ef9127b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d23358cfa109bd18ee1d",
+    "url": "/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "d58a0295d0fe3c21d4c9",
+    "url": "/static/css/main.0a2688a3.chunk.css"
+  },
+  {
+    "revision": "d23358cfa109bd18ee1d",
+    "url": "/static/js/2.70837622.chunk.js"
+  },
+  {
+    "revision": "d58a0295d0fe3c21d4c9",
+    "url": "/static/js/main.58c60e72.chunk.js"
+  },
+  {
+    "revision": "283ecaea64ec162954a9",
+    "url": "/static/js/runtime-main.d0a9e0c7.js"
+  },
+  {
+    "revision": "7165c11a6d8eb5b7603ff1e55e0d7d72",
+    "url": "/static/media/banner.7165c11a.jpg"
+  },
+  {
+    "revision": "2a87eedaaa18e483ba04f0e565df1a1f",
+    "url": "/static/media/graphics.2a87eeda.svg"
+  },
+  {
+    "revision": "704e74849e80be9cdc356d5f1c2ea050",
+    "url": "/static/media/mobile.704e7484.svg"
+  },
+  {
+    "revision": "ff877fee8d3d69a086d2daae55a7f74f",
+    "url": "/static/media/my-image.ff877fee.jpg"
+  },
+  {
+    "revision": "f599bd96d0ddf7dcec79676b24f20962",
+    "url": "/static/media/navlogo.f599bd96.svg"
+  },
+  {
+    "revision": "d6db46a970f10d7612213d5e4ed72ed7",
+    "url": "/static/media/navlogoScroll.d6db46a9.svg"
+  },
+  {
+    "revision": "3515765f8235b5f2fe302334ba570cc5",
+    "url": "/static/media/web.3515765f.svg"
+  }
+]);
