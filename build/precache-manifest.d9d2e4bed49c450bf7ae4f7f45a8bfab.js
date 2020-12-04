65981d6ac0a75b8fd672ad8649deea0b202922e6
RidoySarker <csridoy42@gmail.com>
2020-12-04 15:20:21 +0600

    cv update

---
+++ build/precache-manifest.d9d2e4bed49c450bf7ae4f7f45a8bfab.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11a92075c0026861bb996bacfd61239",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a46dd65625efa5e4f47d",
+    "url": "/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "8f403ad64ba082e27bcf",
+    "url": "/static/css/main.aef1e58e.chunk.css"
+  },
+  {
+    "revision": "a46dd65625efa5e4f47d",
+    "url": "/static/js/2.059bbc58.chunk.js"
+  },
+  {
+    "revision": "8f403ad64ba082e27bcf",
+    "url": "/static/js/main.939482e0.chunk.js"
+  },
+  {
+    "revision": "283ecaea64ec162954a9",
+    "url": "/static/js/runtime-main.d0a9e0c7.js"
+  },
+  {
+    "revision": "2a87eedaaa18e483ba04f0e565df1a1f",
+    "url": "/static/media/graphics.2a87eeda.svg"
+  },
+  {
+    "revision": "704e74849e80be9cdc356d5f1c2ea050",
+    "url": "/static/media/mobile.704e7484.svg"
+  },
+  {
+    "revision": "67c54d952a1b1ba7e342d70dff32a2af",
+    "url": "/static/media/my-image.67c54d95.jpg"
+  },
+  {
+    "revision": "f599bd96d0ddf7dcec79676b24f20962",
+    "url": "/static/media/navlogo.f599bd96.svg"
+  },
+  {
+    "revision": "d6db46a970f10d7612213d5e4ed72ed7",
+    "url": "/static/media/navlogoScroll.d6db46a9.svg"
+  },
+  {
+    "revision": "3515765f8235b5f2fe302334ba570cc5",
+    "url": "/static/media/web.3515765f.svg"
+  }
+]);
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,86 +1,88 @@
-
 const portfolioData = {
-	"name": 'Ridoy Chandra Sarker',
-	"projects": [
-		{
-			"id": 1,			
-			"title": "Pharmacy Management System",
-			"imageUrl": "../../Image/Project/pms.png",
-			"link": "http://demo.bdpriceinfo.com",
-			"codeLink": "https://github.com/RidoySarker/pharmacy",
-			"description": "Pharmacy Management System With Laravel",
-			"technology": ["HTML/CSS", "Bootstrap", "Jquery/Ajax", "Laravel"]				
-		},
-		{
-			"id": 2,			
-			"title": "Park Any Where",
-			"imageUrl": "../../Image/Project/park.png",
-			"link": "",
-			"codeLink": "https://github.com/RidoySarker/Park_Any_Where",
-			"description": "Park Management System With Laravel",
-			"technology": ["HTML/CSS", "Bootstrap", "Jquery/Ajax", "Laravel"]				
-		}							
-		],
-    "skills": [
-		{
-			"name": "HTML/CSS",
-			"percentage": 80
-		},
-		{
-			"name": "SASS/ SCSS",
-			"percentage": 40
-		},	
-		{
-			"name": "Vue js",
-			"percentage": 50
-		},							
-		{
-			"name": "Jquery",
-			"percentage": 80
-		},
+  name: 'Ridoy Chandra Sarker',
+  projects: [
+    {
+      id: 1,
+      title: 'Pharmacy Management System',
+      imageUrl: '../../Image/Project/pms.png',
+      link: 'https://demo.bdpriceinfo.com/',
+      blink: 'https://demo.bdpriceinfo.com/',
+      codeLink: 'https://github.com/RidoySarker/pharmacy',
+      description: 'Pharmacy Management System With Laravel',
+      technology: ['HTML/CSS', 'Bootstrap', 'Jquery/Ajax', 'Laravel']
+    },
+    {
+      id: 2,
+      title: 'Online Parking System',
+      imageUrl: '../../Image/Project/park.png',
+      link: 'https://parkanywhere.bdpriceinfo.com/',
+      blink: 'https://parkanywhere.bdpriceinfo.com/',
+      codeLink: 'https://github.com/RidoySarker/Park_Any_Where',
+      description: 'Park Management System With Laravel',
+      technology: ['HTML/CSS', 'Bootstrap', 'Jquery/Ajax', 'Laravel']
+    }
+  ],
+  skills: [
+    {
+      name: 'HTML/CSS',
+      percentage: 80
+    },
+    {
+      name: 'SASS/ SCSS',
+      percentage: 40
+    },
+    {
+      name: 'Vue js',
+      percentage: 50
+    },
+    {
+      name: 'Jquery',
+      percentage: 80
+    },
 
-		{
-			"name": "REACT",
-			"percentage": 50
-		},
-		{
-			"name": "PHP (LARAVEL)",
-			"percentage": 70
-		},						
-	],
-	"education": [
-		{
-			"title": "Diploma in Data Telecommunication And Networking Technology",
-			"date" : '2016 - 2020',
-			"institute": 'Feni Computer Institute.',
-			"description": "New Ranir Hat, Kazirbag., Feni Rd, 3900 <br/> Result : 3.14 out of 4.00",
-		},
-		{
-			"title": "Secondary School Certificate",
-			"date" : '2010 - 2016',
-			"institute": 'Suagonj T.A High School & College',
-			"description": "Suagazi Bazar School & College Road, Cumilla Sadar Dakshin, 3400 <br/> Result : 5.00 out of 5.00(Science)"
-		},				
-	],
-	
-	"work": [
-		{
-			"title": "Web Application Developer",
-			"date" : 'March 2020- Present',
-			"company": 'Software Developer at SoftArch, Shyamoli (Intern)',
-			"description": "Junior Programmer &nbsp;&nbsp; <li>Working on FTP Movie-Sever Application</li><li>Working on E-Commerce Application</li> 	&nbsp;	&nbsp;	&nbsp;Link: <a href='http://girlyshopper.com'> http://girlyshopper.com</a> ",
-		}				
-	],
+    {
+      name: 'REACT',
+      percentage: 50
+    },
+    {
+      name: 'PHP (LARAVEL)',
+      percentage: 70
+    }
+  ],
+  education: [
+    {
+      title: 'Diploma in Data Telecommunication And Networking Technology',
+      date: '2016 - 2020',
+      institute: 'Feni Computer Institute.',
+      description: 'New Ranir Hat, Kazirbag., Feni Rd, 3900 <br/> Result : 3.14 out of 4.00'
+    },
+    {
+      title: 'Secondary School Certificate',
+      date: '2010 - 2016',
+      institute: 'Suagonj T.A High School & College',
+      description:
+        'Suagazi Bazar School & College Road, Cumilla Sadar Dakshin, 3400 <br/> Result : 5.00 out of 5.00(Science)'
+    }
+  ],
 
-	"courses": [
-		{
-			"title": "About PHP and Laravel Framework",
-			"date" : 'February 2020- May 2020',
-			"company": 'TMSS ICT LTD',
-			"description": "631/5, West Kazipara, Mirpur-10, Dhaka-1216",
-		}				
-	]	
+  work: [
+    {
+      title: 'Web Application Developer',
+      date: 'March 2020- Present',
+      company: 'Software Developer at SoftArch, Shyamoli (Intern)',
+      description:
+        "Junior Programmer &nbsp;&nbsp; <li>Working on Inventory Management Software</li><li>Working on E-Commerce Application</li> 	&nbsp;	&nbsp;	&nbsp;Link: <a href='http://girlyshopper.com'> http://girlyshopper.com</a> "
+    }
+  ],
 
-}
+  courses: [
+    {
+      title: 'About PHP and Laravel Framework',
+      date: 'February 2020- May 2020',
+      company: 'TMSS ICT LTD',
+      description: '631/5, West Kazipara, Mirpur-10, Dhaka-1216'
+    }
+  ]
+};
 
 export default portfolioData;
